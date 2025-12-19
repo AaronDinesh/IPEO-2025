7c98e6e59ba22bcdbf830f1d7de9ee029c0e7d31
Aaron Dinesh <aarondinesh2002@gmail.com>
2025-12-19 19:18:57 +0000

    Added lstm encoder

--- src/models/ssm/encoders.py
+++ src/models/ssm/encoders.py
@@ -133,3 +133,34 @@ class RNNEncoder(AbstractSSMEncoder):
         output, h_n = self.layers[-1](torch.cat([input, state], dim=1))
 
         return output
+
+class LSTMEncoder(AbstractSSMEncoder):
+    def __init__(
+        self,
+        state_space_dim: int,
+        in_features: int,
+        hidden_layers: Tuple[int],
+        activation: Callable = F.relu,
+    ):
+        super().__init__(state_space_dim)
+
+        self.activation = activation
+
+        dim_layers = [in_features] + list(hidden_layers) + [self.state_space_dim,]
+
+        self.layers = nn.ModuleList()
+        for i, (inDim, outDim) in enumerate(zip(dim_layers, dim_layers[1:])):
+            # The state is concatenated to the input of the last layer
+            if i == len(dim_layers)-2:
+                self.layers.append(nn.LSTM(inDim + self.state_space_dim, outDim, batch_first=True))
+            else:
+                self.layers.append(nn.LSTM(inDim, outDim, batch_first=True))
+
+    def forward(self, state: Tensor, input: Tensor) -> Tensor:
+        for layer in self.layers[:-1]:
+            out, tups = layer(input)
+            input = self.activation(out)
+
+        output, tups = self.layers[-1](torch.cat([input, state], dim=1))
+
+        return output
